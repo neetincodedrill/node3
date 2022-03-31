9bc86b28a9a5a04eb7c85486af19be5912bc3db8
codeyash02 <yash.codedrill@gmail.com>
2022-03-31 15:55:04 +0530

    new data

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,57 @@
 const http = require('http');
 const MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb+srv://neetinnegi:neetinnegi@cluster0.tyxfy.mongodb.net/mydb?retryWrites=true&w=majority'
+var url = 'mongodb://localhost:27017/'
 const bodyParser = require('body-parser');
+var formidable = require('formidable');
 var jsonParser = bodyParser.json();
 
 const requestHandler = (req,res) => {
-    if(req.method === 'POST'){
-        var data 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'POST');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Content-Type','multipart/form-data')
+    
+    if(req.method === 'POST'&& req.url === '/user'){
+        var data ;
+        let form=new formidable.IncomingForm();    
         jsonParser(req,res,(err) => {
+        form.parse(req,function(err,fields,files){
         MongoClient.connect(url,function(err,db){
             if(err) throw err;
             var dbo = db.db('mydb');
              data = {
+                image:files,
                 first_name: req.body.first_name,
                 last_name: req.body.last_name,
                 age: req.body.age,
                 gender: req.body.gender,
                 city: req.body.city,
-                email:req.body.email,
-                file: req.file.path
+                email:req.body.email
             }
             console.log(data)
-            dbo.collection('user').insertOne(data,
-            function(err,result){
-                if(err) throw err;
-                res.writeHead(200,"OK",{'Content-Type':'multipart/form-data'})
-                console.log(result)
-            }    
-            )
+            if(data.first_name && data.last_name && data.age && data.gender && data.city && data.email&& data.image){
+                return  dbo.collection('user').insertOne(data,
+                    function(err,result){        
+                        console.log(result)
+                        if(err){
+                            console.log(err)
+                        }else{
+                            res.writeHead(200,"ok")
+                            res.write(result)
+                        }
+                    })
+               }          
+            })
         })
-     })
-     res.end('User data collected')
-    }  
+        })
+    res.end('User data collected')
+    }
 }
 
 const server =  http.createServer(requestHandler)
 
-const port = 7000;
+const port = 3000;
 const host = 'localhost';
 server.listen(port,host)
 console.log(`Server is running at localhost:${port}`)

bd60961c1adcfad8b2685a9f85e08c4c0eccf0ce
codeyash02 <yash.codedrill@gmail.com>
2022-04-01 12:13:16 +0530

    final data

--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,53 @@
 const http = require('http');
 const MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/'
-const bodyParser = require('body-parser');
 var formidable = require('formidable');
-var jsonParser = bodyParser.json();
 
 const requestHandler = (req,res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // console.log(jsonParser);
+  res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'POST');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    // res.setHeader('Content-Type','multipart/form-data')
-    
-    if(req.method === 'POST'){
-        // let form=new formidable.IncomingForm();    
-        jsonParser(req,res,(err) => {
-        // form.parse(req,function(err,fields,files){
-        MongoClient.connect(url,function(err,db){
-            if(err) throw err;
-            var dbo = db.db('mydb');
-            var  data = {
-                // image:files,
-                first_name: req.body.first_name,
-                last_name: req.body.last_name,
-                age: req.body.age,
-                gender: req.body.gender,
-                city: req.body.city,
-                email:req.body.email
+  res.setHeader('Content-Type','multipart/form-data') 
+    if(req.method === "POST"){
+     console.log("hi");
+        const form = formidable({ multiples: true });
+
+    form.parse(req, (err, fields, files) => {  
+        console.log(fields);
+        console.log(files);
+      if (err) {
+        res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
+        res.end(String(err));
+        return;
+      }
+      MongoClient.connect(url,function(err,db){
+        if(err) throw err;
+        var dbo = db.db('mydb');
+        var data = {
+          image : files,
+          field :fields
+        }
+        if(data.image){
+          return dbo.collection('user').insertOne(data,
+            function(err,result){
+              if(err) throw err;
+              res.writeHead(200, { 'Content-Type': 'application/json' });
+              console.log(result)
             }
-            console.log(data)
-            if(data.first_name && data.last_name && data.age && data.gender && data.city && data.email&& data.image){
-                return  dbo.collection('user').insertOne(data,
-                    function(err,result){        
-                        console.log(result)
-                        if(err){
-                            console.log(err)
-                        }else{
-                            res.writeHead(200,"ok")
-                            res.write(result)
-                        }
-                    })
-               }          
-            })
-        })
-        // })
-    res.end('User data collected')
-    }
+            )
+        }
+      
+      res.end(JSON.stringify({ fields, files }, null, 2));
+      })      
+    });
+     }
 }
 
 const server =  http.createServer(requestHandler)
 
-const port = 7000;
+const port = 8000;
 const host = 'localhost';
 server.listen(port,host)
 console.log(`Server is running at localhost:${port}`)
